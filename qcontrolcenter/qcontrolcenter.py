bfe0b416d20f54db680b9003373ba9701924c2ca
Didier Spaier <didier@slint.fr>
2016-11-14 00:28:32 +0100

    Change the location of the config and use pyxdg for parsing the .desktop files and finding the icons.

--- qcontrolcenter/qcontrolcenter.py
+++ qcontrolcenter/qcontrolcenter.py
@@ -1,21 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import sys, os#, string
+# Copyright Thierry Deseez 2008
+# Copyright Didier Spaier 2016
+import sys, os, shutil
+import xdg.DesktopEntry
+import xdg.IconTheme
+# import xdg.Exceptions as exc
+# import xdg.BaseDirectory as bd
 from PyQt4 import QtCore, QtGui
 from helpDialog import helpDialog
 
 # *** SOME GENERAL FUNCTIONS ***
+# We recommend that the sysadmin or distribution maintainer installs the
+# base structure as /etc/skel/.qcontrolcenter
+# In that case a user created afetr that wil get a copy of this structue as
+# ~/.qcontrolcenter
 def getBaseStructureDirectoryPath():
-	base_dir = "/etc/qcontrolcenter"
-	tmp_dir = ""
-	
-	if len(sys.argv) > 1:
-		tmp_dir =  sys.argv[len(sys.argv)-1]
+	home_dir = os.path.expanduser("~")
+	base_dir = os.path.join(home_dir,".qcontrolcenter")
+	if len(sys.argv) == 2:
+		tmp_dir = sys.argv[len(sys.argv)-1]
 		if os.path.exists( tmp_dir ):
 			base_dir = tmp_dir
-	
 	return base_dir
+# The window title is set to the argument of the command case occuring,
+# else to the value of the environment variable QCONTROLCENTERTITLE if set
+# else to qControlCenter.
+# 
+
+def getWindowTitle():
+	window_title = "qControlCenter"
+	if os.getenv('QCONTROLCENTERTITLE') != None:
+		window_title = os.getenv('QCONTROLCENTERTITLE')
+	return window_title
 
 def getLanguage():
 	lng = str(QtCore.QLocale.system().name()).split("_")[0]
@@ -24,156 +41,92 @@ def getLanguage():
 	return lng
 
 # *** GLOBAL VARIABLES ***
-#APP_CONF = QtCore.QSettings("qcontrolcenter")
-#CONFIG_DIR = str(APP_CONF.value("base_directory").toString())
-CONFIG_DIR = getBaseStructureDirectoryPath()#"/etc/archcontrol"
-#APP_CONF.setValue("basedir", QtCore.QVariant(CONFIG_DIR))
+CONFIG_DIR = getBaseStructureDirectoryPath()
 PIXMAPS_DIR = "/usr/share/pixmaps"
 ICONS_DIR = "/usr/share/icons"
-KDE_DIR = unicode(os.getenv('KDEDIR'))
 FOLDER_INI = "folder.ini"
 FOLDER_ICON = "icon.png"
 LANG = getLanguage()
-TRUC = " [qControlCenter]"
-
-#if not APP_CONF.value("basedir").toString().isEmpty():
-	#CONFIG_DIR = unicode(APP_CONF.value("basedir").toString())
-
+TITLE = getWindowTitle()
+TRUC = "qControlCenter"
 print TRUC, "Started ..."
-print TRUC, "Base strutcure directory used =", CONFIG_DIR
+print TRUC, "window title =", TITLE
+print TRUC, "tree =", CONFIG_DIR
 print TRUC, "Language used =", LANG
+if not os.path.exists(CONFIG_DIR):
+	if os.path.exists('/etc/skel/.qcontrolcenter'):
+		shutil.copytree('/etc/skel/.qcontrolcenter',CONFIG_DIR)
+	else:
+		print "No base structure directory found."
+		print "Please read the instructions in this document:"
+		print "/usr/share/qcontrolcenter/INSTRUCTIONS.txt"
 
 # *** MAIN CLASS ***
+"""
+layout:
+	dock (QtGui.QDockWindow)
+		# List widget docked on the left on the windows
+		# It will site the categories (sub-directories of base_dir)
+		self.contentsWidget (Qtgui.QlistWidget(dock))
+	self.textBrowser (QtGui.QTextBroowser)
+		# Text browser in the left space in the window, hnece one the
+		# right of the lsit widget.
+		self.setCentralWidget (self.textBrowser)"""
 class ConfigDialog(QtGui.QMainWindow):
 	def __init__(self):
 		QtGui.QMainWindow.__init__(self)
-
 		self.process = QtCore.QProcess()
-		
-		dock = QtGui.QDockWidget(self.tr("Categories"), self)
+		dock = QtGui.QDockWidget(self.tr(u"Catégories"), self)
+# The dock be non movable, non closable and non floatable by the user. 
 		dock.setFeatures(QtGui.QDockWidget.NoDockWidgetFeatures)
-		font = dock.font()
-		font.setBold(True)
-		dock.setFont( QtGui.QFont(font) )
-		#dock.setAllowedAreas(QtCore.Qt.LeftDockWidgetArea | QtCore.Qt.RightDockWidgetArea)
-		
+		dock.font().setBold(True)
+		dock.setFont( QtGui.QFont(dock.font()) )
 		self.contentsWidget = QtGui.QListWidget(dock)
-		self.contentsWidget.setMaximumWidth(200)
 		self.contentsWidget.setSpacing(1)
 		self.contentsWidget.setAlternatingRowColors(True)
-		#self.contentsWidget.setViewMode(QtGui.QListView.IconMode)
-		#self.contentsWidget.setIconSize(QtCore.QSize(48, 48))
-		#self.contentsWidget.setMovement(QtGui.QListView.Static)
-		
-		#dock2 = QtGui.QDockWidget(self)
-		#dock2.setFeatures(QtGui.QDockWidget.NoDockWidgetFeatures)
-		#self.label = QtGui.QDialogButtonBox(QtGui.QDialogButtonBox.Ok, QtCore.Qt.Horizontal, dock2)
-
-		
+		self.contentsWidget.setIconSize(QtCore.QSize(32, 32))
 		self.textBrowser = QtGui.QTextBrowser()
 		self.textBrowser.setOpenLinks(False)
-
 		self.setCentralWidget(self.textBrowser)
-		
-		self.connect(self.process, QtCore.SIGNAL("finished(int)"), self.processFinished)
+# When an URL is clicked in the central widget (text widget),
+# the corresponding program is launched
 		self.connect(self.textBrowser, QtCore.SIGNAL("anchorClicked(const QUrl &)"), self.launchProgram)
+# When the item (category) in the list widget is changed we change the
+# page seen in the text browser, that will include information taken from
+# the .desktop files in that category (folder in CONFIG_DIR).
 		self.connect(self.contentsWidget, QtCore.SIGNAL("currentItemChanged(QListWidgetItem *, QListWidgetItem *)"), self.changePage)
-		#self.connect(self.contentsWidget, QtCore.SIGNAL("currentTextChanged(const QString &)"), self.changePage)
-		
+		# initialize (populates) the main window
+		# This populates the dock with the list of categories
 		self.initContentsWidget()
+		# Set the initially  highligted category to be the first one.
 		self.contentsWidget.setCurrentRow(0)
-		
+		# set the widget for the dock widget to the list of categories
+		# whose we just defined the layout in initContentsWidget.
+		# This implicitely shows (displays) the widget as the dock widget
+		# was not yet visible
 		dock.setWidget(self.contentsWidget)
-		
-		helpButton = QtGui.QPushButton(QtGui.QIcon("icons/info.png"),"")
-		quitButton = QtGui.QPushButton(QtGui.QIcon("icons/exit.png"),"")
+		helpButton = QtGui.QPushButton(QtGui.QIcon("/usr/share/qcontrolcenter/icons/info.png"),"")
+		quitButton = QtGui.QPushButton(QtGui.QIcon("/usr/share/qcontrolcenter/icons/exit.png"),"")
 		self.statusBar().addPermanentWidget(helpButton, 0)
 		self.statusBar().addPermanentWidget(quitButton, 0)
 		self.connect(helpButton, QtCore.SIGNAL("clicked()"), self.showAbout)
 		self.connect(quitButton, QtCore.SIGNAL("clicked()"), QtCore.SLOT("close()"))
-		
 		self.addDockWidget(QtCore.Qt.LeftDockWidgetArea, dock)
-		#self.addDockWidget(QtCore.Qt.BottomDockWidgetArea, dock2)
-		self.statusBar().showMessage(self.tr("> Select a program"), 0)
-		self.setWindowTitle(self.tr("qControlCenter"))
+		self.setWindowTitle(self.tr(getWindowTitle()))
 		self.setWindowIcon(QtGui.QIcon("icons/arch-logo.png"))
 		self.setMinimumSize(640,480)
-		
-		#if not os.path.exists(CONFIG_DIR):
-			#self.createStructure()
-		
-		#print "Directory structure used :", CONFIG_DIR
 		
 	def showAbout(self):
 		helpDlg = helpDialog(self)
 		helpDlg.show()
-	
-	def createStructure(self):
-		global CONFIG_DIR
-
-		text, ok = QtGui.QInputDialog.getText(
-										self,
-										self.tr("Create Directory Strucure ?"),
-										self.tr(
-											"No directory structure found.\n"
-											"Please provide a base strucutre directory :"
-											),
-										QtGui.QLineEdit.Normal,
-										CONFIG_DIR
-										)
-		if ok and not text.isEmpty():
-			if not os.path.exists(text):
-				if os.mkdir(text):
-					CONFIG_DIR = text
-				else:
-					print TRUC, "could not create directory", text
-					self.close()
-				
-			folder = QtCore.QDir(CONFIG_DIR)
-			folder.setFilter(QtCore.QDir.AllDirs)
-			if folder.entryList().count() == 0:
-				jojo = ["System", "Software", "Devices", "Regional"]
-				for jo in jojo:
-					os.mkdir( os.path.join(CONFIG_DIR, jo) )
-		else:
-			self.close()
 		
 	def launchProgram(self, programName):
-		self.process.start(programName.toString())
-		
-		if not self.process.waitForStarted():
-			print TRUC, "Error : program %s could not be started." % ( str(programName.toString()) )
-			self.statusBar().showMessage(self.tr("program '%1' could not be started.").arg(programName.toString()), 0)
-			return
-		
-		print TRUC, "Program %s started ..." % ( str(programName.toString()) )
-		
-		QtGui.QApplication.setOverrideCursor(QtCore.Qt.BusyCursor)
-		self.setDisabled(True)
-		self.contentsWidget.setDisabled(True)
-		self.textBrowser.setDisabled(True)
-		self.statusBar().showMessage(self.tr("Program '%1' launched ...").arg(programName.toString()), 0)
-	
-	def processFinished(self):
-		print TRUC, "Program ended."
-		
-		QtGui.QApplication.restoreOverrideCursor()
-		self.setDisabled(False)
-		self.contentsWidget.setDisabled(False)
-		self.textBrowser.setDisabled(False)
-		self.statusBar().showMessage(self.tr("> Select a program"), 0)
+		self.process.startDetached(programName.toString())
 
 	def changePage(self):
 		# init values
 		#self.contentsWidget.currentItem().setSelected(True)
 		sectionName = self.contentsWidget.currentItem().data(QtCore.Qt.UserRole).toString()
-		commentName = ""
-		commentNameLng = ""
-		genericName = ""
-		genericNameLng = ""
-		programName = ""
-		execName = ""
-		iconName = ""
 		html = ""
 		current_dir = os.path.join( CONFIG_DIR, unicode(sectionName) )
 		current_column = 1
@@ -181,58 +134,62 @@ class ConfigDialog(QtGui.QMainWindow):
 		# retrieve program informations from '.desktop' files
 		folder = QtCore.QDir(current_dir)
 		folder.setFilter(QtCore.QDir.Files)
+		# filename include all info about each file in the current
+		# directory, whose name is set as the current (highlighted) item
+		# i.e. category in the dock. 
 		for fileName in folder.entryInfoList():
-			if fileName.fileName().endsWith(".desktop"):
-				current_file_name = os.path.join( current_dir, unicode( fileName.fileName() ) )
+			if not fileName.fileName().endsWith(".desktop"):
+				continue
+			current_file_name = os.path.join( current_dir, unicode( fileName.fileName() ) )
+			# Store the settings found in the .desktop file
+			settings = xdg.DesktopEntry.DesktopEntry(filename=os.path.join( current_dir, unicode( fileName.fileName() ) ))
+			onlyShowIn = settings.getOnlyShowIn()
+			notShowIn = settings.getNotShowIn()
+			if 'XDG_CURRENT_DESKTOP' in os.environ and len(onlyShowIn) >0 and not os.environ['XDG_CURRENT_DESKTOP'] in onlyShowIn:
+				continue
+			if 'XDG_CURRENT_DESKTOP' in os.environ and len(notShowIn) >0 and os.environ['XDG_CURRENT_DESKTOP'] in notShowIn:
+				continue
+			if not 'XDG_CURRENT_DESKTOP' in os.environ and len(onlyShowIn) >0:
+				continue
+			beginGroup = settings.get("Desktop Entry")
+			execName = settings.getExec()
+			commentName = settings.getComment()
+			genericName = settings.getGenericName()
+			programName = settings.getName()
+			iconFileName = settings.getIcon()
+			# We display the first found icon among the themes list below.
+			# TODO: allow to provide a list of themes in a configuration file
+			# like /etc/slaptgetrc, that could also set the window title.
+			# As is, the themes are those shipped in Slint.
+			iconName = xdg.IconTheme.getIconPath(iconFileName,64, "Adwaita")
+			if (iconName == None):
+				# package oxygen-icons
+				iconName = xdg.IconTheme.getIconPath(iconFileName,64, "oxygen")
+			if (iconName == None):
+				# nuvola is shipped in kdeartwork
+				iconName = xdg.IconTheme.getIconPath(iconFileName,64, "nuvola")
+			if (iconName == None):
+				# packages tango-icon-themes{,-extra} 
+				iconName = xdg.IconTheme.getIconPath(iconFileName,64, "Tango")
+			if (iconName == None):
+				# hicolor is shipped in gnome_themes_standard
+				# It is the default.
+				iconName = xdg.IconTheme.getIconPath(iconFileName,64, "hicolor")
+			# build cells in html page
+			if current_column == 1:
+				html += (
+						"<tr>" +
+						self.cellBuilder(iconName, programName, commentName, execName) +
+						"<td width='10%'>&nbsp;</td>"
+						)
+			elif current_column == 2:
+				html += (
+						self.cellBuilder(iconName, programName, commentName, execName) +
+						"</tr><tr><td colspan='5'><hr></td></tr>"
+						)
 
-				settings = QtCore.QSettings(current_file_name, QtCore.QSettings.IniFormat)
-				settings.beginGroup("Desktop Entry")
-				execName = settings.value("Exec").toString()
-				commentName = settings.value("Comment").toString()
-				genericName = settings.value("GenericName").toString()
-				programName = settings.value("Name").toString()
-				iconName = settings.value("Icon").toString()
-				if len(LANG) == 2:
-					commentNameLng = unicode(settings.value("Comment[" + LANG + "]").toString(), "utf-8")
-					genericNameLng = unicode(settings.value("GenericName[" + LANG + "]").toString(), "utf-8")
-				settings.endGroup()
-				
-				if len(commentNameLng) > 0:
-					commentName = commentNameLng
-				elif len(genericNameLng) > 0:
-					commentName = genericNameLng
-				 
-				if len(commentName) < 1:
-					commentName = genericName
-				"""
-				if len(LANG) > 1:
-					if ( settings.value("Comment[" + LANG + "]").toString() ) != "":
-						commentName = unicode(settings.value("Comment[" + LANG + "]").toString(), "utf-8")
-					elif ( settings.value("GenericName[" + LANG + "]").toString() ) != "":
-						commentName = unicode(settings.value("GenericName[" + LANG + "]").toString(), "utf-8")
-				if len(commentName) < 1:
-					commentName = settings.value("GenericName").toString()
-				settings.endGroup()
-				"""
-				
-				# build icon name
-				iconName = self.iconNameBuilder(iconName)
-				
-				# build cells in html page
-				if current_column == 1:
-					html += (
-							"<tr>" +
-							self.cellBuilder(iconName, programName, commentName, execName) +
-							"<td width='10%'>&nbsp;</td>"
-							)
-				elif current_column == 2:
-					html += (
-							self.cellBuilder(iconName, programName, commentName, execName) +
-							"</tr><tr><td colspan='5'><hr></td></tr>"
-							)
-
-				if current_column == 1 : current_column += 1
-				elif current_column == 2: current_column -= 1
+			if current_column == 1 : current_column += 1
+			elif current_column == 2: current_column -= 1
 				
 		if current_column == 2:
 			html += "</tr><tr><td colspan='5'><hr></td></tr>"
@@ -256,37 +213,6 @@ class ConfigDialog(QtGui.QMainWindow):
 				"</table>"
 				)
 
-	def iconNameBuilder(self, name):
-		# this is a full path icon name
-		if name.contains("/"):
-			return name
-		
-		# check extension
-		if name.indexOf(".", name.size()-4) < 0:
-			name.append(".png")#print "index of" , name, "found"
-		
-		# guess icon path from multiple possible locations
-		nameFound = "icons/unknown.png"
-		locations =[
-					os.path.join(PIXMAPS_DIR, unicode(name)),
-					os.path.join(ICONS_DIR, unicode(name)),
-					os.path.join(ICONS_DIR + "/hicolor/48x48/apps", unicode(name)),
-					os.path.join(ICONS_DIR + "/hicolor/32x32/apps", unicode(name)),
-					os.path.join(KDE_DIR + "/share/icons/default.kde/48x48/apps", unicode(name)),
-					os.path.join(KDE_DIR + "/share/icons/default.kde/32x32/apps", unicode(name)),
-					os.path.join(KDE_DIR + "/share/icons/hicolor/48x48/apps", unicode(name)),
-					os.path.join(KDE_DIR + "/share/icons/hicolor/32x32/apps", unicode(name)),
-					os.path.join(KDE_DIR + "/share/icons/kdeclassic/48x48/apps", unicode(name)),
-					os.path.join(KDE_DIR + "/share/icons/kdeclassic/32x32/apps", unicode(name))
-					]
-		
-		for location in locations:
-			if os.path.exists(location):
-				nameFound = location
-				break
-		
-		return nameFound
-
 	def cellBuilder(self, iconName, programName, commentName, execName):
 		cell = (
 				u"<td width='60' class='joe' valign='middle' align='left'>"
@@ -298,25 +224,25 @@ class ConfigDialog(QtGui.QMainWindow):
 				)
 		
 		return cell
-
+    # This method populates the list of categories in the left hand dock.
 	def initContentsWidget(self):
 		folder = QtCore.QDir(CONFIG_DIR)
 		folder.setFilter(QtCore.QDir.AllDirs)
-		
+		# Loop inside the categories in the config tree
 		for folderName in folder.entryInfoList():
+			# Condider only category names of al laast three characters
 			if folderName.fileName().size() > 2:
 				current_dir = os.path.join( CONFIG_DIR, unicode(folderName.fileName()) )
-				
+				# Bear in mind that self.contentsWidget is a docked list item
+				# configButton is an item in that list
 				configButton = QtGui.QListWidgetItem( self.contentsWidget)
 				configButton.setIcon( QtGui.QIcon( os.path.join(current_dir, FOLDER_ICON) ) )
 				configButton.setText( self.getSectionTitle(current_dir) )#configButton.setText(folderName.fileName())
 				configButton.setTextAlignment( QtCore.Qt.AlignLeft )
+				# Align center vertically (Didier)
+				configButton.setTextAlignment( QtCore.Qt.AlignVCenter )
 				configButton.setData( QtCore.Qt.UserRole, QtCore.QVariant( folderName.fileName()) )
-				#configButton.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
-				#configButton.setFlags( QtCore.Qt.ItemIsEnabled)
-				font = configButton.font()
-				font.setBold(True)
-				configButton.setFont( QtGui.QFont(font) )
+				configButton.setFont(QtGui.QFont("DejaVu Sans", 11, QtGui.QFont.Bold))
 
 	def getSectionTitle(self, dir_name):
 		titleList = unicode(dir_name).split("/")
